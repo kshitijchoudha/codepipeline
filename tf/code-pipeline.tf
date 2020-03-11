80f8138d17fded621c1791f192383374986403ab
kshitijchoudha <kshitij.choudha@gmail.com>
2020-03-10 21:36:45 -0500

    update pipeline

--- tf/code-pipeline.tf
+++ tf/code-pipeline.tf
@@ -104,7 +104,7 @@ resource "aws_codepipeline" "codepipeline" {
       run_order = "1"
       version   = "1"
       configuration = {
-        ImageTag       = "tf-dev"
+        ImageTag       = "tf-dev1"
         RepositoryName = "nginx"
       }
       output_artifacts = ["TestImage"]
